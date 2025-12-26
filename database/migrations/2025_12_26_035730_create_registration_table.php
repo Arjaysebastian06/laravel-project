aad4a6c5e357099ac8cb9c433196425e76dde808
Arjaysebastian06 <arjaysebastian0606@gmail.com>
2025-12-26 12:59:37 +0800

    Create migration for registration table with necessary fields

---
+++ database/migrations/2025_12_26_035730_create_registration_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registration', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('email_address')->unique();
+            $table->date('date_of_birth');
+            $table->string('password');     
+            $table->string('password_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registration');
+    }
+}
